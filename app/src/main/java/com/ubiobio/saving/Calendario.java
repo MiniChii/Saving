d0767f3409e29940c296fe2efa8b48888800be79
JuanMEMOUBB <43252326+JuanMEMOUBB@users.noreply.github.com>
2019-05-31 02:25:30 -0400

    toolbar bonito

--- app/src/main/java/com/ubiobio/saving/Calendario.java
+++ app/src/main/java/com/ubiobio/saving/Calendario.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Calendario extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class Calendario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fabVolver);
         fab.setOnClickListener(new View.OnClickListener() {
